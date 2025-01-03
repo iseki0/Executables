2c78786b780c3bf9a95bf57bb336f7cc8c9ef0a3
iseki <admin@iseki.space>
2025-01-04 06:05:07 +0800

    fix(common): remove @JvmName annotation on "expect" .kt file
    It should be written in the "actual" file

--- src/commonMain/kotlin/space/iseki/executables/pe/U.kt
+++ src/commonMain/kotlin/space/iseki/executables/pe/U.kt
@@ -1,7 +1,4 @@
-@file:JvmName(" U")
 package space.iseki.executables.pe
-
-import kotlin.jvm.JvmName
 
 internal expect fun ByteArray.getUShort(offset: Int): UShort
 internal expect fun ByteArray.getUInt(offset: Int): UInt
