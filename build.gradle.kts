3ba37733ff9223920f802312161939c3ea7d823f
iseki <admin@iseki.space>
2025-01-04 05:47:05 +0800

    build: publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import java.util.*
 
 plugins {
     kotlin("multiplatform") version "2.1.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.17.0"
     kotlin("plugin.serialization") version "2.1.0"
+    `maven-publish`
+    signing
 }
 
 allprojects {
@@ -43,12 +46,16 @@ tasks.withType<AbstractArchiveTask> {
 }
 
 tasks.withType<Jar> {
-    manifest {
-        attributes["Automatic-Module-Name"] = "space.iseki.executables"
+    if ("jvm" in name) {
+        manifest {
+            attributes["Automatic-Module-Name"] = "space.iseki.executables"
+        }
     }
-    into("/") {
-        from("/LICENSE")
-        from("/NOTICE")
+    if ("emptyJavadocJar" !in name) {
+        into("/") {
+            from("/LICENSE")
+            from("/NOTICE")
+        }
     }
 }
 
@@ -56,4 +63,73 @@ tasks.withType<Test> {
     testLogging {
         events("passed", "skipped", "failed")
     }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Central"
+            afterEvaluate {
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    // uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    uri("https://oss.sonatype.org/content/repositories/snapshots")
+                } else {
+                    // uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                    uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+                }
+            }
+            credentials {
+                username = properties["ossrhUsername"]?.toString() ?: System.getenv("OSSRH_USERNAME")
+                password = properties["ossrhPassword"]?.toString() ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
+    publications {
+        withType<MavenPublication> {
+            val pubName = name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else "$it" }
+            val emptyJavadocJar by tasks.register<Jar>("emptyJavadocJar$pubName") {
+                archiveClassifier = "javadoc"
+                archiveBaseName = artifactId
+            }
+            artifact(emptyJavadocJar)
+            pom {
+                val projectUrl = "https://github.com/iseki0/Executables"
+                name = "Executables"
+                description = "A library for executable files"
+                url = projectUrl
+                licenses {
+                    license {
+                        name = "Apache-2.0"
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "iseki0"
+                        name = "iseki zero"
+                        email = "iseki@iseki.space"
+                    }
+                }
+                inceptionYear = "2024"
+                scm {
+                    connection = "scm:git:$projectUrl.git"
+                    developerConnection = "scm:git:$projectUrl.git"
+                    url = projectUrl
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "$projectUrl/issues"
+                }
+                ciManagement {
+                    system = "GitHub"
+                    url = "$projectUrl/actions"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
