2a8b1018181cee05f4e19fde5ce2534d7c010add
iseki <admin@iseki.space>
2025-01-04 05:11:28 +0800

    fix(jvm): unused code in U

--- src/jvmMain/kotlin/space/iseki/executables/pe/U.jvm.kt
+++ src/jvmMain/kotlin/space/iseki/executables/pe/U.jvm.kt
@@ -2,8 +2,6 @@ package space.iseki.executables.pe
 
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.VarHandle
-import java.net.ServerSocket
-import java.net.Socket
 import java.nio.ByteOrder
 
 private val SH: VarHandle =
@@ -12,10 +10,6 @@ private val IH: VarHandle =
     MethodHandles.byteArrayViewVarHandle(IntArray::class.java, ByteOrder.LITTLE_ENDIAN).withInvokeExactBehavior()
 private val LH: VarHandle =
     MethodHandles.byteArrayViewVarHandle(LongArray::class.java, ByteOrder.LITTLE_ENDIAN).withInvokeExactBehavior()
-
-fun a(s: Socket){
-    s.getInputStream().available()
-}
 
 internal actual fun ByteArray.getUShort(offset: Int): UShort {
     return (SH.get(this, offset) as Short).toUShort()
