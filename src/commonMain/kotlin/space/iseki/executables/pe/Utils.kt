459c3e6af656440eeae22891be7413367c3cdb7c
iseki <admin@iseki.space>
2025-01-22 08:24:18 +0800

    refactor: remove unused serializer

--- src/commonMain/kotlin/space/iseki/executables/pe/Utils.kt
+++ src/commonMain/kotlin/space/iseki/executables/pe/Utils.kt
@@ -2,14 +2,8 @@
 
 package space.iseki.executables.pe
 
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerializationException
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.serialDescriptor
-import kotlinx.serialization.encoding.CompositeDecoder
 import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.encoding.decodeStructure
 import kotlin.jvm.JvmName
 
 internal fun decodeLongHex(decoder: Decoder, name: String): Long {
@@ -24,51 +18,3 @@ internal fun decodeLongHex(decoder: Decoder, name: String): Long {
     }
 }
 
-internal abstract class BitSetSerializer<T : Set<T>>(
-    private val max: ULong,
-    private val name: String,
-    private val plusFn: (T, T) -> T,
-) : KSerializer<T> {
-    override val descriptor: SerialDescriptor
-        get() = serialDescriptor<List<String>>()
-
-    protected abstract val unit: T
-    protected abstract fun valueOfOrNull(element: String): T?
-    protected abstract fun valueOf(element: ULong): T
-    private fun valueOf0(element: String): T {
-        return valueOfOrNull(element) ?: run {
-            if (!element.startsWith("0x")) {
-                throw SerializationException("$name should be a predefined value or a hex value")
-            }
-            val v = try {
-                element.substring(2..element.lastIndex).toULong()
-            } catch (e: NumberFormatException) {
-                throw SerializationException("$name should be a predefined value or a hex value", e)
-            }
-            if (v > max) {
-                throw SerializationException("hex value too large for a $name")
-            }
-            valueOf(v)
-        }
-    }
-
-    override fun deserialize(decoder: Decoder): T = decoder.decodeStructure(serialDescriptor<List<String>>()) {
-        var r = unit
-        while (true) {
-            val index = decodeElementIndex(descriptor)
-            if (index == CompositeDecoder.DECODE_DONE) break
-            r = plusFn(r, valueOf0(decodeStringElement(descriptor, index)))
-        }
-        r
-    }
-
-
-    override fun serialize(encoder: Encoder, value: T) {
-        encoder.beginCollection(serialDescriptor<List<String>>(), value.size).apply {
-            for ((index, characteristics) in value.withIndex()) {
-                encodeStringElement(descriptor, index, characteristics.toString())
-            }
-            endStructure(descriptor)
-        }
-    }
-}
