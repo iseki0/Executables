8f833b3ffc89f4ec55b9b4cc30d6e9177e8140da
iseki <admin@iseki.space>
2025-01-22 12:50:45 +0800

    build: only nonJvm targets require ktxs-json

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,16 +44,15 @@ kotlin {
 
     sourceSets {
         val commonMain by getting
-        val jsMain by getting {
-            dependencies {
-                implementation(libs.kotlinx.serialization.json)
-            }
-        }
+        val jsMain by getting
         val wasmJsMain by getting
         val nonJvmMain by creating {
             dependsOn(commonMain)
             jsMain.dependsOn(this)
             wasmJsMain.dependsOn(this)
+            dependencies {
+                implementation(libs.kotlinx.serialization.json)
+            }
         }
     }
 }
