e410d7fa46588a69a840160bef713ae6e4442cb9
iseki <admin@iseki.space>
2025-02-18 01:39:02 +0800

    docs(pe): add kdoc of TimeDateStamp32

--- pe/src/commonMain/kotlin/space/iseki/executables/pe/TimeDataStamp32.kt
+++ pe/src/commonMain/kotlin/space/iseki/executables/pe/TimeDataStamp32.kt
@@ -10,6 +10,19 @@ import kotlinx.serialization.encoding.Encoder
 import kotlin.jvm.JvmInline
 import kotlin.jvm.JvmStatic
 
+/**
+ * Represents a 32-bit time stamp in PE/COFF file format.
+ *
+ * The low 32 bits of the number of seconds since 00:00 January 1, 1970 (a C run-time time_t value),
+ * which indicates when the file was created.
+ *
+ * This value is commonly used in various PE/COFF structures:
+ * - COFF File Header: indicates when the file was created
+ * - Import Directory: date/time stamp of the DLL to import
+ * - Debug Directory: date/time stamp of the debug information
+ *
+ * @property rawValue The raw 32-bit value representing seconds since Unix epoch (00:00 January 1, 1970).
+ */
 @Serializable(with = TimeDataStamp32.Serializer::class)
 @JvmInline
 value class TimeDataStamp32(val rawValue: UInt) : Comparable<TimeDataStamp32> {
@@ -31,11 +44,22 @@ value class TimeDataStamp32(val rawValue: UInt) : Comparable<TimeDataStamp32> {
         return rawValue.compareTo(other.rawValue)
     }
 
+    /**
+     * Converts the time stamp to its string representation in ISO-8601 format.
+     *
+     * @return The ISO-8601 formatted date-time string representing this time stamp.
+     */
     override fun toString(): String {
         return Instant.fromEpochSeconds(rawValue.toLong()).toString()
     }
 
     companion object {
+        /**
+         * Converts a raw time stamp value to its string representation in ISO-8601 format.
+         *
+         * @param rawValue The raw time stamp value as Int.
+         * @return The ISO-8601 formatted date-time string representing the time stamp.
+         */
         @JvmStatic
         fun toString(rawValue: Int): String {
             return TimeDataStamp32(rawValue.toUInt()).toString()
