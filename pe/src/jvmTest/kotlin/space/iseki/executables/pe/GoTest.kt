f3946be349ad5dc0c89a4b0a3e2f7f6bae14bc21
iseki <admin@iseki.space>
2025-02-19 21:43:17 +0800

    test(pe): add executable type detection assertion

--- pe/src/jvmTest/kotlin/space/iseki/executables/pe/GoTest.kt
+++ pe/src/jvmTest/kotlin/space/iseki/executables/pe/GoTest.kt
@@ -5,11 +5,14 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromStream
 import kotlinx.serialization.json.encodeToJsonElement
+import space.iseki.executables.common.ByteArrayDataAccessor
+import space.iseki.executables.common.ExecutableFile
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 private val classLoader = GoTest::class.java.classLoader
 private val data = classLoader.getResourceAsStream("go.exe")!!.use { it.readAllBytes() }
+
 @OptIn(ExperimentalSerializationApi::class)
 private val summaryJson =
     classLoader.getResourceAsStream("go.exe.summary.json")!!.use { Json.decodeFromStream<JsonElement>(it) }
@@ -19,6 +22,7 @@ class GoTest {
 
     @Test
     fun test() {
+        assertEquals(ExecutableFile.PE, ExecutableFile.detect(ByteArrayDataAccessor(data)))
         PEFile.wrap(data).use { pe ->
             assertEquals(summaryJson, json.encodeToJsonElement(pe.summary))
             println(json.encodeToString(pe.versionInfo?.stringFileInfo?.strings))
