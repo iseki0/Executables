69399de0a1ec83aeae8ad5d8d9ee253760c294ec
iseki <admin@iseki.space>
2025-03-02 01:25:07 +0800

    feat(common): import table abstraction

---
+++ common/src/commonMain/kotlin/space/iseki/executables/common/ImportSymbol.kt
@@ -0,0 +1,24 @@
+package space.iseki.executables.common
+
+/**
+ * Represents an imported symbol in an executable file
+ *
+ * @property name The name of the imported symbol
+ * @property file The name of the file (library) from which the symbol is imported
+ */
+interface ImportSymbol {
+    val name: String
+    val file: String
+}
+
+/**
+ * Represents a container that holds import symbols
+ *
+ * This interface is implemented by executable file formats that support
+ * importing symbols from external libraries.
+ *
+ * @property importSymbols The list of import symbols in this container
+ */
+interface ImportSymbolContainer {
+    val importSymbols: List<ImportSymbol>
+}
