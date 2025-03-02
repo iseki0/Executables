05270fbd706655004f6cf1b214655ca6a1b9b2e3
iseki <admin@iseki.space>
2025-03-03 05:05:42 +0800

    refactor(common): reinterpret API use MH on Java 9

--- common/src/commonMain/kotlin/space/iseki/executables/common/ByteArrayUtils.kt
+++ common/src/commonMain/kotlin/space/iseki/executables/common/ByteArrayUtils.kt
@@ -10,7 +10,7 @@ import kotlin.jvm.JvmName
  * Gets an unsigned byte at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u1(offset: Int): UByte = (this[offset].toUInt() and 0xFFu).toUByte()
+expect fun ByteArray.u1(offset: Int): UByte
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -18,8 +18,7 @@ fun ByteArray.u1(offset: Int): UByte = (this[offset].toUInt() and 0xFFu).toUByte
  * Gets an unsigned short in little-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u2l(offset: Int): UShort =
-    ((this[offset].toUInt() and 0xFFu) or ((this[offset + 1].toUInt() and 0xFFu) shl 8)).toUShort()
+expect fun ByteArray.u2l(offset: Int): UShort
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -27,8 +26,7 @@ fun ByteArray.u2l(offset: Int): UShort =
  * Gets an unsigned int in little-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u4l(offset: Int): UInt =
-    ((this[offset].toUInt() and 0xFFu) or ((this[offset + 1].toUInt() and 0xFFu) shl 8) or ((this[offset + 2].toUInt() and 0xFFu) shl 16) or ((this[offset + 3].toUInt() and 0xFFu) shl 24))
+expect fun ByteArray.u4l(offset: Int): UInt
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -36,8 +34,7 @@ fun ByteArray.u4l(offset: Int): UInt =
  * Gets an unsigned long in little-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u8l(offset: Int): ULong =
-    ((this[offset].toULong() and 0xFFu) or ((this[offset + 1].toULong() and 0xFFu) shl 8) or ((this[offset + 2].toULong() and 0xFFu) shl 16) or ((this[offset + 3].toULong() and 0xFFu) shl 24) or ((this[offset + 4].toULong() and 0xFFu) shl 32) or ((this[offset + 5].toULong() and 0xFFu) shl 40) or ((this[offset + 6].toULong() and 0xFFu) shl 48) or ((this[offset + 7].toULong() and 0xFFu) shl 56))
+expect fun ByteArray.u8l(offset: Int): ULong
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -45,8 +42,7 @@ fun ByteArray.u8l(offset: Int): ULong =
  * Gets an unsigned short in big-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u2b(offset: Int): UShort =
-    (((this[offset].toUInt() and 0xFFu) shl 8) or (this[offset + 1].toUInt() and 0xFFu)).toUShort()
+expect fun ByteArray.u2b(offset: Int): UShort
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -54,8 +50,7 @@ fun ByteArray.u2b(offset: Int): UShort =
  * Gets an unsigned int in big-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u4b(offset: Int): UInt =
-    (((this[offset].toUInt() and 0xFFu) shl 24) or ((this[offset + 1].toUInt() and 0xFFu) shl 16) or ((this[offset + 2].toUInt() and 0xFFu) shl 8) or (this[offset + 3].toUInt() and 0xFFu))
+expect fun ByteArray.u4b(offset: Int): UInt
 
 /**
  * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
@@ -63,5 +58,4 @@ fun ByteArray.u4b(offset: Int): UInt =
  * Gets an unsigned long in big-endian order at the specified offset.
  */
 @ExeInternalApi
-fun ByteArray.u8b(offset: Int): ULong =
-    (((this[offset].toULong() and 0xFFu) shl 56) or ((this[offset + 1].toULong() and 0xFFu) shl 48) or ((this[offset + 2].toULong() and 0xFFu) shl 40) or ((this[offset + 3].toULong() and 0xFFu) shl 32) or ((this[offset + 4].toULong() and 0xFFu) shl 24) or ((this[offset + 5].toULong() and 0xFFu) shl 16) or ((this[offset + 6].toULong() and 0xFFu) shl 8) or (this[offset + 7].toULong() and 0xFFu))
+expect fun ByteArray.u8b(offset: Int): ULong
---
+++ common/src/jvmJpms/kotlin/space/iseki/executables/common/ByteArrayUtils.kt
@@ -0,0 +1,59 @@
+@file:JvmName("-ByteArrayUtils")
+
+package space.iseki.executables.common
+
+import java.lang.invoke.MethodHandles
+import java.nio.ByteOrder
+
+private val SHORT_VIEW_LE = MethodHandles.byteArrayViewVarHandle(ShortArray::class.java, ByteOrder.LITTLE_ENDIAN)
+private val SHORT_VIEW_BE = MethodHandles.byteArrayViewVarHandle(ShortArray::class.java, ByteOrder.BIG_ENDIAN)
+private val INT_VIEW_LE = MethodHandles.byteArrayViewVarHandle(IntArray::class.java, ByteOrder.LITTLE_ENDIAN)
+private val INT_VIEW_BE = MethodHandles.byteArrayViewVarHandle(IntArray::class.java, ByteOrder.BIG_ENDIAN)
+private val LONG_VIEW_LE = MethodHandles.byteArrayViewVarHandle(LongArray::class.java, ByteOrder.LITTLE_ENDIAN)
+private val LONG_VIEW_BE = MethodHandles.byteArrayViewVarHandle(LongArray::class.java, ByteOrder.BIG_ENDIAN)
+
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned byte at the specified offset.
+ */
+fun ByteArray.u1(offset: Int): UByte = this[offset].toUByte()
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned short in little-endian order at the specified offset.
+ */
+fun ByteArray.u2l(offset: Int): UShort = (SHORT_VIEW_LE.get(this, offset) as Short).toUShort()
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned int in little-endian order at the specified offset.
+ */
+fun ByteArray.u4l(offset: Int): UInt = (INT_VIEW_LE.get(this, offset) as Int).toUInt()
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned long in little-endian order at the specified offset.
+ */
+fun ByteArray.u8l(offset: Int): ULong = (LONG_VIEW_LE.get(this, offset) as Long).toULong()
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned short in big-endian order at the specified offset.
+ */
+fun ByteArray.u2b(offset: Int): UShort = (SHORT_VIEW_BE.get(this, offset) as Short).toUShort()
+
+/**
+ * INTERNAL API - DO NOT USE OUTSIDE OF THIS PROJECT.
+ *
+ * Gets an unsigned int in big-endian order at the specified offset.
+ */
+fun ByteArray.u4b(offset: Int): UInt = (INT_VIEW_BE.get(this, offset) as Int).toUInt()
+
+
+fun ByteArray.u8b(offset: Int): ULong = (LONG_VIEW_BE.get(this, offset) as Long).toULong()
