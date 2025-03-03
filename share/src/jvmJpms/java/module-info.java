5329e544e49d9bd1d124afc032d013b6c61888e6
iseki <admin@iseki.space>
2025-03-03 23:54:13 +0800

    fix(jigsaw): exports to -> exports

--- share/src/jvmJpms/java/module-info.java
+++ share/src/jvmJpms/java/module-info.java
@@ -1,4 +1,4 @@
 module space.iseki.executables.share {
     requires transitive kotlin.stdlib;
-    exports space.iseki.executables.share to space.iseki.executables.pe, space.iseki.executables.elf;
+    exports space.iseki.executables.share;
 }
