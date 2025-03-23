2b19e767aff3a5126efea443f339d28ddedc274c
iseki <admin@iseki.space>
2025-03-23 16:50:22 +0800

    feat(macho): add JPMS supports

---
+++ macho/src/jvmJpms/java/module-info.java
@@ -0,0 +1,8 @@
+module space.iseki.executables.macho {
+    requires transitive kotlin.stdlib;
+    requires transitive space.iseki.executables.common;
+    requires transitive kotlinx.serialization.core;
+    requires kotlinx.datetime;
+    requires space.iseki.executables.share;
+    exports space.iseki.executables.macho;
+}
