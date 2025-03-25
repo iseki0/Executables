cd63353814f2eb71f9d498b3323d9584b54d5d7c
iseki <admin@iseki.space>
2025-03-25 11:55:52 +0800

    build: remove unused compiler argument

--- buildSrc/src/main/kotlin/convention.gradle.kts
+++ buildSrc/src/main/kotlin/convention.gradle.kts
@@ -31,7 +31,6 @@ kotlin {
         compilerOptions {
             freeCompilerArgs.add("-Xexpect-actual-classes")
             freeCompilerArgs.add("-Xconsistent-data-class-copy-visibility")
-            freeCompilerArgs.add("-opt-in=space.iseki.executables.common.ExeInternalApi")
         }
         jvm {
             compilerOptions {
