7832e64830a2ed3319822a26bd85fbf5ed5c6d04
iseki <admin@iseki.space>
2025-04-04 02:19:53 +0800

    fix(macho): MachoFileException should be a CommonFileException

--- files/src/commonMain/kotlin/space/iseki/executables/macho/MachoFileException.kt
+++ files/src/commonMain/kotlin/space/iseki/executables/macho/MachoFileException.kt
@@ -1,11 +1,11 @@
 package space.iseki.executables.macho
 
-import space.iseki.executables.common.IOException
+import space.iseki.executables.common.CommonFileException
 
 /**
  * Exception thrown when a Mach-O file is invalid or cannot be processed.
  */
-class MachoFileException : IOException {
+class MachoFileException : CommonFileException {
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable) : super(message, cause)
 }
