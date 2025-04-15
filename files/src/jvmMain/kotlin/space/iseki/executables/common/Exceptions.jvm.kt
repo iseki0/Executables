c054e7cdd3bb087052ddbc612532e17a695e81d0
iseki <admin@iseki.space>
2025-04-16 02:34:51 +0800

    fix: remove unused function

--- files/src/jvmMain/kotlin/space/iseki/executables/common/Exceptions.jvm.kt
+++ files/src/jvmMain/kotlin/space/iseki/executables/common/Exceptions.jvm.kt
@@ -4,7 +4,3 @@ actual typealias EOFException = java.io.EOFException
 actual typealias IOException = java.io.IOException
 actual typealias NoSuchFileException = java.nio.file.NoSuchFileException
 actual typealias AccessDeniedException = java.nio.file.AccessDeniedException
-
-fun a() {
-
-}
