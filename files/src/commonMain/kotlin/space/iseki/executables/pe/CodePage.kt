1e6170445f697a34a13d39a545e7042ef7e88d81
iseki <admin@iseki.space>
2025-06-18 23:20:57 +0800

    perf(pe): optimize CodePage name table

--- files/src/commonMain/kotlin/space/iseki/executables/pe/CodePage.kt
+++ files/src/commonMain/kotlin/space/iseki/executables/pe/CodePage.kt
@@ -22,162 +22,146 @@ value class CodePage(val value: UInt) : Comparable<CodePage> {
         get() = codePageRef[value]
 }
 
-private val codePageRef by lazy(LazyThreadSafetyMode.PUBLICATION) {
-    val txt = """
-    37	IBM037
-    437	IBM437
-    500	IBM500
-    708	ASMO-708
-    709
-    710
-    720	DOS-720
-    737	ibm737
-    775	ibm775
-    850	ibm850
-    852	ibm852
-    855	IBM855
-    857	ibm857
-    858	IBM00858
-    860	IBM860
-    861	ibm861
-    862	DOS-862
-    863	IBM863
-    864	IBM864
-    865	IBM865
-    866	cp866
-    869	ibm869
-    870	IBM870
-    874	windows-874
-    875	cp875
-    932	shift_jis
-    936	gb2312
-    949	ks_c_5601-1987
-    950	big5
-    1026	IBM1026
-    1047	IBM01047
-    1140	IBM01140
-    1141	IBM01141
-    1142	IBM01142
-    1143	IBM01143
-    1144	IBM01144
-    1145	IBM01145
-    1146	IBM01146
-    1147	IBM01147
-    1148	IBM01148
-    1149	IBM01149
-    1200	utf-16
-    1201	unicodeFFFE
-    1250	windows-1250
-    1251	windows-1251
-    1252	windows-1252
-    1253	windows-1253
-    1254	windows-1254
-    1255	windows-1255
-    1256	windows-1256
-    1257	windows-1257
-    1258	windows-1258
-    1361	Johab
-    10000	macintosh
-    10001	x-mac-japanese
-    10002	x-mac-chinesetrad
-    10003	x-mac-korean
-    10004	x-mac-arabic
-    10005	x-mac-hebrew
-    10006	x-mac-greek
-    10007	x-mac-cyrillic
-    10008	x-mac-chinesesimp
-    10010	x-mac-romanian
-    10017	x-mac-ukrainian
-    10021	x-mac-thai
-    10029	x-mac-ce
-    10079	x-mac-icelandic
-    10081	x-mac-turkish
-    10082	x-mac-croatian
-    12000	utf-32
-    12001	utf-32BE
-    20000	x-Chinese_CNS
-    20001	x-cp20001
-    20002	x_Chinese-Eten
-    20003	x-cp20003
-    20004	x-cp20004
-    20005	x-cp20005
-    20105	x-IA5
-    20106	x-IA5-German
-    20107	x-IA5-Swedish
-    20108	x-IA5-Norwegian
-    20127	us-ascii
-    20261	x-cp20261
-    20269	x-cp20269
-    20273	IBM273
-    20277	IBM277
-    20278	IBM278
-    20280	IBM280
-    20284	IBM284
-    20285	IBM285
-    20290	IBM290
-    20297	IBM297
-    20420	IBM420
-    20423	IBM423
-    20424	IBM424
-    20833	x-EBCDIC-KoreanExtended
-    20838	IBM-Thai
-    20866	koi8-r
-    20871	IBM871
-    20880	IBM880
-    20905	IBM905
-    20924	IBM00924
-    20932	EUC-JP
-    20936	x-cp20936
-    20949	x-cp20949
-    21025	cp1025
-    21027
-    21866	koi8-u
-    28591	iso-8859-1
-    28592	iso-8859-2
-    28593	iso-8859-3
-    28594	iso-8859-4
-    28595	iso-8859-5
-    28596	iso-8859-6
-    28597	iso-8859-7
-    28598	iso-8859-8
-    28599	iso-8859-9
-    28603	iso-8859-13
-    28605	iso-8859-15
-    29001	x-Europa
-    38598	iso-8859-8-i
-    50220	iso-2022-jp
-    50221	csISO2022JP
-    50222	iso-2022-jp
-    50225	iso-2022-kr
-    50227	x-cp50227
-    50229
-    50930
-    50931
-    50933
-    50935
-    50936
-    50937
-    50939
-    51932	euc-jp
-    51936	EUC-CN
-    51949	euc-kr
-    51950
-    52936	hz-gb-2312
-    54936	GB18030
-    57002	x-iscii-de
-    57003	x-iscii-be
-    57004	x-iscii-ta
-    57005	x-iscii-te
-    57006	x-iscii-as
-    57007	x-iscii-or
-    57008	x-iscii-ka
-    57009	x-iscii-ma
-    57010	x-iscii-gu
-    57011	x-iscii-pa
-    65000	utf-7
-    65001	utf-8
-""".trimIndent()
-    val pattern = Regex("\\s+")
-    txt.lineSequence().map { it.split(pattern) }.filter { it.size == 2 }.map { it[0].toUInt() to it[1] }.toMap()
-}
+private val codePageRef = mapOf(
+    37u to "IBM037",
+    437u to "IBM437",
+    500u to "IBM500",
+    708u to "ASMO-708",
+    720u to "DOS-720",
+    737u to "ibm737",
+    775u to "ibm775",
+    850u to "ibm850",
+    852u to "ibm852",
+    855u to "IBM855",
+    857u to "ibm857",
+    858u to "IBM00858",
+    860u to "IBM860",
+    861u to "ibm861",
+    862u to "DOS-862",
+    863u to "IBM863",
+    864u to "IBM864",
+    865u to "IBM865",
+    866u to "cp866",
+    869u to "ibm869",
+    870u to "IBM870",
+    874u to "windows-874",
+    875u to "cp875",
+    932u to "shift_jis",
+    936u to "gb2312",
+    949u to "ks_c_5601-1987",
+    950u to "big5",
+    1026u to "IBM1026",
+    1047u to "IBM01047",
+    1140u to "IBM01140",
+    1141u to "IBM01141",
+    1142u to "IBM01142",
+    1143u to "IBM01143",
+    1144u to "IBM01144",
+    1145u to "IBM01145",
+    1146u to "IBM01146",
+    1147u to "IBM01147",
+    1148u to "IBM01148",
+    1149u to "IBM01149",
+    1200u to "utf-16",
+    1201u to "unicodeFFFE",
+    1250u to "windows-1250",
+    1251u to "windows-1251",
+    1252u to "windows-1252",
+    1253u to "windows-1253",
+    1254u to "windows-1254",
+    1255u to "windows-1255",
+    1256u to "windows-1256",
+    1257u to "windows-1257",
+    1258u to "windows-1258",
+    1361u to "Johab",
+    10000u to "macintosh",
+    10001u to "x-mac-japanese",
+    10002u to "x-mac-chinesetrad",
+    10003u to "x-mac-korean",
+    10004u to "x-mac-arabic",
+    10005u to "x-mac-hebrew",
+    10006u to "x-mac-greek",
+    10007u to "x-mac-cyrillic",
+    10008u to "x-mac-chinesesimp",
+    10010u to "x-mac-romanian",
+    10017u to "x-mac-ukrainian",
+    10021u to "x-mac-thai",
+    10029u to "x-mac-ce",
+    10079u to "x-mac-icelandic",
+    10081u to "x-mac-turkish",
+    10082u to "x-mac-croatian",
+    12000u to "utf-32",
+    12001u to "utf-32BE",
+    20000u to "x-Chinese_CNS",
+    20001u to "x-cp20001",
+    20002u to "x_Chinese-Eten",
+    20003u to "x-cp20003",
+    20004u to "x-cp20004",
+    20005u to "x-cp20005",
+    20105u to "x-IA5",
+    20106u to "x-IA5-German",
+    20107u to "x-IA5-Swedish",
+    20108u to "x-IA5-Norwegian",
+    20127u to "us-ascii",
+    20261u to "x-cp20261",
+    20269u to "x-cp20269",
+    20273u to "IBM273",
+    20277u to "IBM277",
+    20278u to "IBM278",
+    20280u to "IBM280",
+    20284u to "IBM284",
+    20285u to "IBM285",
+    20290u to "IBM290",
+    20297u to "IBM297",
+    20420u to "IBM420",
+    20423u to "IBM423",
+    20424u to "IBM424",
+    20833u to "x-EBCDIC-KoreanExtended",
+    20838u to "IBM-Thai",
+    20866u to "koi8-r",
+    20871u to "IBM871",
+    20880u to "IBM880",
+    20905u to "IBM905",
+    20924u to "IBM00924",
+    20932u to "EUC-JP",
+    20936u to "x-cp20936",
+    20949u to "x-cp20949",
+    21025u to "cp1025",
+    21866u to "koi8-u",
+    28591u to "iso-8859-1",
+    28592u to "iso-8859-2",
+    28593u to "iso-8859-3",
+    28594u to "iso-8859-4",
+    28595u to "iso-8859-5",
+    28596u to "iso-8859-6",
+    28597u to "iso-8859-7",
+    28598u to "iso-8859-8",
+    28599u to "iso-8859-9",
+    28603u to "iso-8859-13",
+    28605u to "iso-8859-15",
+    29001u to "x-Europa",
+    38598u to "iso-8859-8-i",
+    50220u to "iso-2022-jp",
+    50221u to "csISO2022JP",
+    50222u to "iso-2022-jp",
+    50225u to "iso-2022-kr",
+    50227u to "x-cp50227",
+    51932u to "euc-jp",
+    51936u to "EUC-CN",
+    51949u to "euc-kr",
+    52936u to "hz-gb-2312",
+    54936u to "GB18030",
+    57002u to "x-iscii-de",
+    57003u to "x-iscii-be",
+    57004u to "x-iscii-ta",
+    57005u to "x-iscii-te",
+    57006u to "x-iscii-as",
+    57007u to "x-iscii-or",
+    57008u to "x-iscii-ka",
+    57009u to "x-iscii-ma",
+    57010u to "x-iscii-gu",
+    57011u to "x-iscii-pa",
+    65000u to "utf-7",
+    65001u to "utf-8",
+).toMap()
 
