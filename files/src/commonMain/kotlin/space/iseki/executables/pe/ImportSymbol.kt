89e08b30afa039acfae2e383fc57ca0e8d4ca743
iseki <admin@iseki.space>
2025-06-19 14:52:18 +0800

    perf(pe): optimize PEImportSymbol memory use

--- files/src/commonMain/kotlin/space/iseki/executables/pe/ImportSymbol.kt
+++ files/src/commonMain/kotlin/space/iseki/executables/pe/ImportSymbol.kt
@@ -1,6 +1,7 @@
 package space.iseki.executables.pe
 
 import space.iseki.executables.common.ImportSymbol
+import kotlin.jvm.JvmName
 
 /**
  * Implementation of import symbol for PE format
@@ -10,17 +11,39 @@ import space.iseki.executables.common.ImportSymbol
  * @property ordinal The ordinal number of the import symbol (if imported by ordinal)
  * @property isOrdinal Whether this symbol is imported by ordinal
  */
-data class PEImportSymbol internal constructor(
+data class PEImportSymbol private constructor(
     override val name: String,
     override val file: String,
-    val ordinal: UShort? = null,
+    private val _ordinal: UShort = 0u,
     val isOrdinal: Boolean = false,
 ) : ImportSymbol {
+    constructor(
+        name: String,
+        file: String,
+        ordinal: UShort? = null,
+        isOrdinal: Boolean = false,
+    ) : this(
+        name = name,
+        file = file,
+        _ordinal = if (isOrdinal) {
+            requireNotNull(ordinal)
+        } else {
+            require(ordinal == null) { "ordinal must be null if isOrdinal is false" }
+            0u
+        },
+        isOrdinal = isOrdinal,
+    )
+
+    @JvmName("component3-XRpZGF0")
+    fun `_component3-XRpZGF0`(): UShort? = ordinal
+
+    val ordinal: UShort? get() = if (isOrdinal) _ordinal else null
+
     override val fields: Map<String, Any>
         get() = buildMap {
             put("name", name)
             put("file", file)
-            if (ordinal != null) put("ordinal", ordinal)
+            if (isOrdinal) put("ordinal", _ordinal)
             put("isOrdinal", isOrdinal)
         }
 }
